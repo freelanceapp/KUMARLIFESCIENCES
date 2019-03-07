aa950ba76faded1c801288d12b975ff554bc4519
Pushpendra Bhati <pushpendra.infobite@gmail.com>
2019-03-07 12:18:09 +0530

    home page changes

--- app/src/main/java/com/infobite/life/ui/activity/SplashScreen.java
+++ app/src/main/java/com/infobite/life/ui/activity/SplashScreen.java
@@ -1,14 +1,41 @@
 package com.infobite.life.ui.activity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.VideoView;
+
+import com.bumptech.glide.Glide;
+import com.infobite.life.utils.BaseActivity;
+import com.squareup.picasso.Picasso;
+
 import infobite.kumar.life.R;
 
-public class SplashScreen extends AppCompatActivity {
+public class SplashScreen extends BaseActivity {
+    private ImageView imgSplash;
+    private VideoView videoView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        imgSplash = findViewById(R.id.img_splash);
+
+        Glide.with(mContext)
+                .load("http://kumarlifesciences.com/images/preloaders/1.gif")
+                .into(imgSplash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(mContext,HomeNavigationActivity.class));
+            }
+        },2000);
+
     }
+
 }
